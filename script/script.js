9a58d11fb11b2d73c6d3d158364a054d95d196a7
Apolo Barua Apurbo <apolo.itnet@gmail.com>
2025-03-01 11:45:18 +0600

    PH-B11-Assignment-5-Button-Function-Commit

--- script/script.js
+++ script/script.js
@@ -1,11 +1,72 @@
 // day and date update function 
 
 const now = new Date();
-
 const day = now.toLocaleString('en-US', { weekday: 'short'});
-
 const date = now.toLocaleString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
-
 
 document.getElementById('day').textContent=day ;
 document.getElementById('date').textContent=date ;
+
+
+const taskButtons = [
+  document.getElementById('task-btn-1'),
+  document.getElementById('task-btn-2'),
+  document.getElementById('task-btn-3'),
+  document.getElementById('task-btn-4'),
+  document.getElementById('task-btn-5'),
+  document.getElementById('task-btn-6'),
+];
+const taskHeadings = [
+  document.getElementById('task-heading-1').textContent,
+  document.getElementById('task-heading-2').textContent,
+  document.getElementById('task-heading-3').textContent,
+  document.getElementById('task-heading-4').textContent,
+  document.getElementById('task-heading-5').textContent,
+  document.getElementById('task-heading-6').textContent,
+];
+const navAddTask = document.getElementById('nav-add-task');
+const assignComplete = document.getElementById('assign-complete');
+const logContainer = document.getElementById('log-container');
+
+let taskCount = 0;
+const totalTask = taskButtons.length;
+
+for (let i = 0; i < taskButtons.length; i++) {
+  taskButtons[i].onclick = function(){
+    taskCount++;
+
+    alert('Board updated Successfully');
+    if (taskCount === totalTask) {
+      alert('congrats!!! You have completed all the current task')
+    } 
+
+    const navValue = parseInt(navAddTask.textContent) || 0;
+    navAddTask.textContent = navValue + 1;
+
+    const assignValue = parseInt(assignComplete.textContent) || 0;
+    if (assignValue > 0){
+      assignComplete.textContent = assignValue - 1;
+    }
+
+    const logContainer = document.getElementById('log-container');
+      if (!logContainer) {
+        console.error("logContainer not found in the document!");
+      }
+
+
+    const currentTime = new Date().toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second:'numeric', hour12: true });
+    const logText = "You have Complete the Task " + taskHeadings[i] + " at " + currentTime;
+    const newLog = document.createElement('p');
+    newLog.textContent = logText;
+    newLog.className = 'text-sm text-gray-600 p-2 m-2 mb-2 rounded-lg bg-blue-50';
+    logContainer.appendChild(newLog);
+
+    taskButtons[i].disabled = true;
+
+  }
+}
+
+const clearButton = document.getElementById('clear-btn');
+clearButton.onclick = function(){
+  logContainer.innerHTML = '';
+};
